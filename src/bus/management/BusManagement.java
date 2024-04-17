790d4253570f9a13eb789aea48ad33fe0022eb9d
lokesh <lokeshmeshram140@gmail.com>
2024-04-17 15:53:48 +0530

    Inital commit

---
+++ src/bus/management/BusManagement.java
@@ -0,0 +1,10 @@
+package bus.management;
+public class BusManagement {
+
+    public static void main(String[] args) {
+        MainScreen mr=new MainScreen();
+        mr.setLocationRelativeTo(null);
+        mr.setVisible(true);
+    }
+    
+}
